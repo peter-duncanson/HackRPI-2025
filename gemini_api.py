38507373025ca21b0250a8f3463a225e9307b359
peter-duncanson <85356486+peter-duncanson@users.noreply.github.com>
2025-11-15 12:26:51 -0500

    Add files via upload

---
+++ gemini_api.py
@@ -0,0 +1,11 @@
+import google.generativeai as genai
+import os
+
+genai.configure(api_key='AIzaSyDfXd192JsXgrc_uUD6aX9XLJZ40YLn81s')
+
+model = genai.GenerativeModel('gemini-2.5-flash')
+
+response = model.generate_content('generate me a dictionary in this python file with general emotions as keys and more specific words for those emotions as values')
+
+print(response.text)
+
